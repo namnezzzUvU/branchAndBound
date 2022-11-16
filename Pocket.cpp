ef277851201bc9c18a1bbc15c82e95d91704698f
chuTuanNam <namchu2311@gmail.com>
2022-11-16 20:17:38 +0700

    new file

---
+++ Pocket.cpp
@@ -0,0 +1,9 @@
+/*
+Một nhà thám hiểm cần đem theo một
+cái túi trọng lượng không quá 𝐵. Có 𝑁 đồ vật cần đem
+theo. Đồ vật thứ 𝑖 có trọng lượng 𝑎𝑖
+, có giá trị sử dụng 𝑐𝑖
+(𝑖 = 1, 2, . . . , 𝑁; 𝑎𝑖 , 𝑐𝑖 ∈ 𝑍+). Hãy tìm cách đưa đồ vật
+vào túi cho nhà thám hiểm sao cho tổng giá trị sử dụng
+các đồ vật trong túi là lớn nhất.
+*/
